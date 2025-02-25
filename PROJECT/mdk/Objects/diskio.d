de1fc09dc67da7b863d8fd98a8a23524dbf51df4
ZhangStudyLife <zyz051013@gmail.com>
2025-02-25 18:47:41 +0800

    Add mdk folder contents to Git

---
+++ PROJECT/mdk/Objects/diskio.d
@@ -0,0 +1,12 @@
+./objects/diskio.o: ../../libraries/components/fatfs/source/diskio.c \
+  ../../libraries/components/fatfs/source/ff.h \
+  ../../libraries/components/fatfs/source/ffconf.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ../../libraries/components/fatfs/source/diskio.h \
+  ../../libraries/zf_device\zf_device_sdcard.h \
+  ../../libraries/zf_common\zf_common_typedef.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdlib.h
---
+++ PROJECT/mdk/Objects/zf_device_gps_tau1201.d
@@ -0,0 +1,15 @@
+./objects/zf_device_gps_tau1201.o: \
+  ../../libraries/zf_device/zf_device_gps_tau1201.c \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\math.h \
+  ../../libraries/zf_common\zf_common_function.h \
+  ../../libraries/zf_common/zf_common_typedef.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  ../../libraries/zf_common\zf_common_fifo.h \
+  ../../libraries/zf_driver\zf_driver_delay.h \
+  ../../libraries/zf_driver\zf_driver_uart.h \
+  ../../libraries/zf_device/zf_device_gps_tau1201.h
