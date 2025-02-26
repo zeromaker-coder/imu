81dc5a45ca14eacad46e1143e683239c9fcca9e1
zeromaker-coder <1849059209@qq.com>
2025-02-26 19:24:17 +0800

    稍微改一下imu解算，接下来去搞卡尔曼滤波，好像这个更稳定一些，不过更难调

--- PROJECT/code/imu.h
+++ PROJECT/code/imu.h
@@ -2,23 +2,25 @@
 #define __IMU_H
 
 			
-			typedef struct{
-				float AX;
-				float AY;
-				float AZ;
-				float GX;
-				float GY;
-				float GZ;
-							}param_imu;
-			
-		    typedef struct{
-				float Pitch;
-				float Roll;
-				float Yaw;
-							}param_Angle;
-			
-			extern param_Angle imu_Angle_Filted;
-			extern param_imu imu_data;
+typedef struct{
+	float AX;
+	float AY;
+	float AZ;
+	float GX;
+	float GY;
+	float GZ;
+				}param_imu;
+
+	typedef struct{
+	float Pitch;
+	float Roll;
+	float Yaw;
+				}param_Angle;
+
+extern param_Angle imu_Angle_Filted;
+extern param_imu imu_data;
 void IMU_getEuleranAngles(void);
+void IMU_Init(void);
 							
 #endif
+
